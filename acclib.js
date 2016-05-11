3eef12c0cc5c525e41ea437046fad1998cbe51d7
brianl <brian.londregan@gmail.com>
2016-05-11 15:19:41 -0400

    reorganized functions, starting a new CheckLabels function

--- acclib.js
+++ acclib.js
@@ -14,7 +14,26 @@ var acclib = (function AccModule() {
 		}
 	}
 	
-
+	function BuildBetterTitle(newTitle) {
+		document.title = newTitle;
+	}
+	
+	function CheckAltTags() {
+		var elems = document.getElementsByTagName('img'); 
+		for(var i = 0; i < elems.length; i++) {
+			var a = elems[i].getAttribute('alt');
+			if( !a || a == "") {
+				console.log(elems[i].outerHTML + " is missing an Alt tag"); 
+			}
+			else if( a.toString().length > 125 ) {
+				console.log(elems[i].outerHTML + " needs a shorter Alt tag"); 
+			}
+		}
+	}
+	
+	function CheckLabels() {
+		console.log("checking for labels...");
+	}
 	
 	function MakeFocusVisible() {
 		var css = 'select:focus, input:focus, textarea:focus, a:focus, button:focus { outline: 2px solid #6699ff !important; }';
@@ -30,36 +49,21 @@ var acclib = (function AccModule() {
 		}
 
 		head.appendChild(style);
-	}
-		
-	function CheckAltTags() {
-		var elems = document.getElementsByTagName('img'); 
-		for(var i = 0; i < elems.length; i++) {
-			var a = elems[i].getAttribute('alt');
-			if( !a || a == "") {
-				console.log(elems[i].outerHTML + " is missing an Alt tag"); 
-			}
-			else if( a.toString().length > 125 ) {
-				console.log(elems[i].outerHTML + " needs a shorter Alt tag"); 
-			}
-		}
-	}
+	}		
+
 	
 	
 	
 	
 	
-	function BuildBetterTitle(newTitle) {
-		document.title = newTitle;
-	}
-		
 	return {
 		AssignRoles: AssignRoles,
-		MakeFocusVisible: MakeFocusVisible,
+		BuildBetterTitle: BuildBetterTitle,
 		CheckAltTags: CheckAltTags,
-		BuildBetterTitle: BuildBetterTitle
-	};			
-		
+		CheckLabels: CheckLabels,
+		MakeFocusVisible: MakeFocusVisible
+	};				
+
 })(); //END AccModule
 
 
