dffbf732cdc0b625138e8e2371d8a494c1bcae7f
Brian Londregan <brian.londregan@gmail.com>
2018-02-08 14:32:43 -0500

    reworked everything for Open text questions

--- aria-tagging.js
+++ aria-tagging.js
@@ -1,59 +1,91 @@
+//FOR THE PAGE
+document.getElementsByClassName("cf-page__hidden-fields")[0].setAttribute("aria-hidden", "true");
+document.getElementsByClassName("cf-page__header")[0].setAttribute("role", "banner");
+document.getElementsByClassName("cf-page__main")[0].setAttribute("role", "main");
+var progBar = document.getElementsByClassName("cf-progress__indicator");
+if(progBar[0]) {
+  var accProgress = document.createEle5ddment("div");
+  var accProgressTxt = document.createTextNode("Current survey progress is " + progBar[0].style.width);
+  accProgress.style.textIndent = "-9999px";
+  accProgress.appendChild(accProgressTxt);
+  progBar[0].parentNode.parentNode.appendChild(accProgress);
+}
+document.getElementsByClassName("cf-page__navigation")[0].setAttribute("role", "navigation");
+var navButtons = document.getElementsByClassName("cf-navigation__button");
+for(var i = 0; i < navButtons.length; i++) {
+  navButtons[i].setAttribute("tabindex", "0");
+  navButtons[i].setAttribute("role", "presentation");
+}
+
 //FOR EACH QUESTION ON PAGE
-Confirmit.page.questions.forEach(function(q) {
+Confirmit.page.questions.forEach(function(q,i) {
+
 
   //GENERAL TO ALL QUESTIONS----------------------------------------------------
-  if( q.title && q.title != "" ) {
+  document.title = (i == 0) ? q.title : document.title;
+  //give each question's container a form role with a label
+  document.getElementById(q.id).setAttribute("role", "form");
+  document.getElementById(q.id).setAttribute("aria-label", q.title);
+  //assign ids for labelling
+  $("div#" + q.id + " div.cf-question__text").attr("id", q.id + "_txt");
+  $("div#" + q.id + " div.cf-question__instruction").attr("id", q.id + "_ins");
+  $("div#" + q.id + " div.cf-question__error").attr("id", q.id + "_err");
 
-    //give each question's container a form role and a label
-    var sel = "div#" + q.id;
-    $(sel).each(function() {
-      $(this).attr({"role":"form", "aria-label":q.title});
-    });
-
-    //give the browser tab a better title
-    var onlyOnce = 1;
-    if(onlyOnce) {
-      document.title = q.title;
-      onlyOnce = 0;
-    }
-  } //------------------------------------------------------------------ALL-----
 
   //SPECIFIC TO OPEN TEXT QUESTIONS---------------------------------------------
   if(q.type == "OpenText") {
-    var inputSel = "#" + q.id + "_input";
-
-    //assign a collective label to each Open Text questions' input
-    $("div#" + q.id + " div.cf-question__text").attr("id", q.id + "_txt");
-    $("div#" + q.id + " div.cf-question__instruction").attr("id", q.id + "_ins");
-    $(inputSel).attr("aria-labelledby", q.id + "_txt" + " " + q.id + "_ins");
-
-    //if required, add required property
+    var input = document.getElementById(q.id + "_input");
+    //assign labels to the Open Text input
+    input.setAttribute("aria-labelledby", q.id + "_txt" + " " + q.id + "_ins");
+    input.setAttribute("aria-errormessage", q.id + "_err");
     if(q.required) {
-      $(inputSel).attr("aria-required", "true");
+      //if required, add required property
+      input.setAttribute("aria-required", "true");
     }
   } //---------------------------------------------------------------OPEN-------
 
-  //SPECIFIC TO SINGLE QUESTIONS------------------------------------------------
-  else if(q.type == "Single") {
 
-
-  } //-------------------------------------------------------------SINGLE-------
-});
-
-//FOR ERROR HANDLING
-var config = { attributes: true };
-var targets = document.getElementsByClassName("cf-toast");
-
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    var mutationNode = mutation.target;
-    if( mutationNode.className.indexOf("cf-toast--hidden") !== -1 ){
-      mutationNode.removeAttribute("role");
-    }
-    else {
-      mutationNode.setAttribute("role", "alert");
-    }
+  //GENERAL TO ALL QUESTIONS----------------------------------------------------
+  var config = { attributes: true, subtree: true };
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if(mutation.attributeName == "class") {
+        var mutationNode = mutation.target;
+        //alert roles should be set and unset on every thrown error area on the page
+        //also set and unset invalid attribute on the input accordingly
+        ToggleAlert(mutationNode, input);
+        ToggleAlert(document.getElementsByClassName("cf-toast")[0], input);
+      }
+    });
   });
+
+  var errorArea = document.getElementsByClassName("cf-question__error")[0]; //must be this current question error area!
+  observer.observe(errorArea, config);
 });
 
-observer.observe(targets[0], config);
+
+
+function ToggleAlert(x, y) {
+  var cl = x.classList.value;
+  if( cl.indexOf("--hidden") !== -1 ) {
+    //if element is hidden...
+    if( x.hasAttribute("role") ) {
+      x.removeAttribute("role");
+      y.removeAttribute("aria-invalid");
+    }
+  }
+  else {
+    //if element is not hidden... (error state)
+    if( !x.hasAttribute("role") ) {
+      x.setAttribute("role", "alert");
+      y.setAttribute("aria-invalid", "true");
+    }
+  }
+}
+
+
+/*var sel = $("#" + q.id + " div.cf-error-block").each( function() { //bug! for grids the selector is not getting all the error areas
+//does not exist at the time this runs! for the grid only. label seems to be added later. make this selector more general
+  observer.observe(this, config);
+});
+*/
